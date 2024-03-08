991540e0db839912665f3692c2909a61bdf64eb8
Ismael Gomes da Silva <ismaelcraft74@gmail.com>
2024-03-08 19:35:12 -0300

    Update solucoes.java

--- solucoes.java
+++ solucoes.java
@@ -23,14 +23,10 @@ public class solucoes{
         scanner.close();*/
 
     //3º) Solução da Questão 3:
-        //a):
-            int valorInicialA = 1;
-            int proximoNumeroA = 0;
-            for(int i = 0; i <= 10; i++){
-                proximoNumeroA = valorInicialA + 2;
-                valorInicialA = proximoNumeroA;
-                System.out.println(proximoNumeroA);
-            }
+        //a): 9. Pois o valor aumenta a cada duas unidades.
+        //b): 128. Pois é a multiplicação do numero anterior por 2.
+        //c): 47.Pois a sequência segue um padrão de soma com numeros impares, ou seja, 1, 3, 5, 7, 9, 11...
+        //d):     
     }
 
     //Função da questão 2:
